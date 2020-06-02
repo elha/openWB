796243da17d2a36e7b5d42fa12e1cd0cf8477174
elha <harald.elsperger@xpecto.com>
2020-06-02 22:20:58 +0200

    sdm630 use register 0x48 as 0x0156 is not available on older models

--- modules/sdm630modbusll/readsdm.py
+++ modules/sdm630modbusll/readsdm.py
@@ -26,7 +26,7 @@ print(struct.unpack('>f',struct.pack('>HH',*resp.registers)))
 
 resp = client.read_input_registers(0x0C,2, unit=sdmid)
 print(struct.unpack('>f',struct.pack('>HH',*resp.registers)))
-resp = client.read_input_registers(0x0156,2, unit=sdmid)
+resp = client.read_input_registers(0x48,2, unit=sdmid)
 print(struct.unpack('>f',struct.pack('>HH',*resp.registers)))
 resp = client.read_input_registers(0x0E,2, unit=sdmid)
 print(struct.unpack('>f',struct.pack('>HH',*resp.registers)))
